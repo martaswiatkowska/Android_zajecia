0d69d1e6bfa4aa3e9fb0547d9ff6041cf3928304
Marta Świątkowska <martaswiatkowska8@gmail.com>
2017-12-26 23:59:43 +0100

    not ended

--- app/src/main/java/com/example/marta/zadanie9/MainActivity.java
+++ app/src/main/java/com/example/marta/zadanie9/MainActivity.java
@@ -1,70 +1,119 @@
 package com.example.marta.zadanie9;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.CheckBox;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    // Elementy Ekranu
-    TextView tv;
-    CheckBox chb;
+    final int MENU_COLOR_RED = 1;
+    final int MENU_COLOR_GREEN = 2;
+    final int MENU_COLOR_BLUE = 3;
+
+    final int MENU_SIZE_22 = 4;
+    final int MENU_SIZE_26 = 5;
+    final int MENU_SIZE_30 = 6;
+
+    TextView tvColor, tvSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lv = (ListView) findViewById(R.id.listview);
 
-        // znajdziemy elementy
-        tv = (TextView) findViewById(R.id.textView);
-        chb = (CheckBox) findViewById(R.id.chbExtMenu);
+        tvColor = (TextView) findViewById(R.id.tvColor);
+        tvSize = (TextView) findViewById(R.id.tvSize);
 
+        //
+        registerForContextMenu(tvColor);
+        registerForContextMenu(tvColor);
+        registerForContextMenu(lv);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
 
-        // dodaj pozycji menu
-        menu.add(0, 1, 0, "add");
-        menu.add(0, 2, 0, "edit");
-        menu.add(0, 3, 3, "delete");
-        menu.add(1, 4, 1, "copy");
-        menu.add(1, 5, 2, "paste");
-        menu.add(1, 6, 4, "exit");
-
-        // metoda N 2 poprzez xml- pliku
-        //getMenuInflater().inflate(R.menu.my_menu, menu);
-
-
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
-    // aktualizacja menu
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // elementy menu z grupy o ID = 1 będą widoczne
-        menu.setGroupVisible(1, chb.isChecked());
-        return super.onPrepareOptionsMenu(menu);
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        switch (v.getId()) {
+            case R.id.tvColor:
+                menu.add(0, MENU_COLOR_RED, 0, "red");
+                menu.add(0, MENU_COLOR_GREEN, 0, "green");
+                menu.add(0, MENU_COLOR_BLUE, 0, "blue");
+                break;
+            case R.id.tvSize:
+                menu.add(0, MENU_SIZE_22, 0, "22");
+                menu.add(0, MENU_SIZE_26, 0, "26");
+                menu.add(0, MENU_SIZE_30, 0, "30");
+                break;
+        }
+
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
     }
 
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // пункты меню для tvColor
+            case MENU_COLOR_RED:
+                tvColor.setTextColor(Color.RED);
+                tvColor.setText("Text color = red");
+                break;
+            case MENU_COLOR_GREEN:
+                tvColor.setTextColor(Color.GREEN);
+                tvColor.setText("Text color = green");
+                break;
+            case MENU_COLOR_BLUE:
+                tvColor.setTextColor(Color.BLUE);
+                tvColor.setText("Text color = blue");
+                break;
+            // пункты меню для tvSize
+            case MENU_SIZE_22:
+                tvSize.setTextSize(22);
+                tvSize.setText("Text size = 22");
+                break;
+            case MENU_SIZE_26:
+                tvSize.setTextSize(26);
+                tvSize.setText("Text size = 26");
+                break;
+            case MENU_SIZE_30:
+                tvSize.setTextSize(30);
+                tvSize.setText("Text size = 30");
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        StringBuilder sb = new StringBuilder();
-
-        // Wpisanie do  TextView informacji o pozycji elementu w menu
-        sb.append("Item Menu");
-        sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
-        sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
-        sb.append("\r\n order: " + String.valueOf(item.getOrder()));
-        sb.append("\r\n title: " + item.getTitle());
-        tv.setText(sb.toString());
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
