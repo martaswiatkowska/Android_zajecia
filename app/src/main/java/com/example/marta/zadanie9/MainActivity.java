9fb51fada234523da581bebb2a956b0e7070797b
Marta Świątkowska <martaswiatkowska8@gmail.com>
2017-12-27 18:09:42 +0100

    zadanie 16

--- app/src/main/java/com/example/marta/zadanie9/MainActivity.java
+++ app/src/main/java/com/example/marta/zadanie9/MainActivity.java
@@ -1,125 +1,70 @@
 package com.example.marta.zadanie9;
 
-import android.app.Activity;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    final int MENU_COLOR_RED = 1;
-    final int MENU_COLOR_GREEN = 2;
-    final int MENU_COLOR_BLUE = 3;
-
-    final int MENU_SIZE_22 = 4;
-    final int MENU_SIZE_26 = 5;
-    final int MENU_SIZE_30 = 6;
-
-    TextView tvColor, tvSize;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ListView lv = (ListView) findViewById(R.id.listview);
 
-        tvColor = (TextView) findViewById(R.id.tvColor);
-        tvSize = (TextView) findViewById(R.id.tvSize);
+        //setContentView(R.layout.activity_main);
 
-        tvColor.setOnLongClickListener(new View.OnLongClickListener() {
+        // tworzenie LinearLayout
+        LinearLayout linLayout = new LinearLayout(this);
+        // ustanowić orientacji pionowej        linLayout.setOrientation(LinearLayout.VERTICAL);
+        // tworzenie LayoutParams
+        LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        // określić linLayout jako katalogu głównym ekranu
+        setContentView(linLayout, linLayoutParam);
 
-            @Override
-            public boolean onLongClick(View v) {
-                // TODO Auto-generated method stub
-                return false;
-            }
-        });
+        LinearLayout.LayoutParams lpView = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //tworzenie elementu textView
+        TextView tv = new TextView(this);
+        //dodawanie tekstu do nowo utworzonego TextView
+        tv.setText("TextView");
+        //ustawienie parametrów textView
+        tv.setLayoutParams(lpView);
+        //dodawanie elementu do LinearLayout
+        linLayout.addView(tv);
 
-        tvSize.setOnLongClickListener(new View.OnLongClickListener() {
+        Button btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
 
-            @Override
-            public boolean onLongClick(View v) {
-                // TODO Auto-generated method stub
-                return false;
-            }
-        });
-        //
-        registerForContextMenu(tvColor);
-        registerForContextMenu(tvColor);
 
-        registerForContextMenu(lv);
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
+
+        Button btn1 = new Button(this);
+        btn1.setText("Button1");
+        linLayout.addView(btn1, leftMarginParams);
+
+
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button btn2 = new Button(this);
+        btn2.setText("Button2");
+        linLayout.addView(btn2, rightGravityParams);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return true;
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        switch (v.getId()) {
-            case R.id.tvColor:
-                menu.add(0, MENU_COLOR_RED, 0, "red");
-                menu.add(0, MENU_COLOR_GREEN, 0, "green");
-                menu.add(0, MENU_COLOR_BLUE, 0, "blue");
-                break;
-            case R.id.tvSize:
-                menu.add(0, MENU_SIZE_22, 0, "22");
-                menu.add(0, MENU_SIZE_26, 0, "26");
-                menu.add(0, MENU_SIZE_30, 0, "30");
-                break;
-        }
-
-
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // пункты меню для tvColor
-            case MENU_COLOR_RED:
-                tvColor.setTextColor(Color.RED);
-                tvColor.setText("Text color = red");
-                break;
-            case MENU_COLOR_GREEN:
-                tvColor.setTextColor(Color.GREEN);
-                tvColor.setText("Text color = green");
-                break;
-            case MENU_COLOR_BLUE:
-                tvColor.setTextColor(Color.BLUE);
-                tvColor.setText("Text color = blue");
-                break;
-            // пункты меню для tvSize
-            case MENU_SIZE_22:
-                tvSize.setTextSize(22);
-                tvSize.setText("Text size = 22");
-                break;
-            case MENU_SIZE_26:
-                tvSize.setTextSize(26);
-                tvSize.setText("Text size = 26");
-                break;
-            case MENU_SIZE_30:
-                tvSize.setTextSize(30);
-                tvSize.setText("Text size = 30");
-                break;
-        }
-        return super.onContextItemSelected(item);
     }
 
     @Override
