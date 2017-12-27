f4b452310a9721f1ccacff64ec46cc5d108d4ae2
Marta Świątkowska <martaswiatkowska8@gmail.com>
2017-12-27 20:52:33 +0100

    zadanie 19

--- app/src/main/java/com/example/marta/zadanie9/MainActivity.java
+++ app/src/main/java/com/example/marta/zadanie9/MainActivity.java
@@ -2,80 +2,126 @@ package com.example.marta.zadanie9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    final int MENU_RESET_ID = 1;
+    final int MENU_QUIT_ID = 2;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
-    SeekBar sbWeight;
-    Button btn1;
-    Button btn2;
-    LinearLayout.LayoutParams lParams1;
-    LinearLayout.LayoutParams lParams2;
+    EditText etNum1;
+    EditText etNum2;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+
+    TextView tvResult;
+
+    String oper = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sbWeight = (SeekBar) findViewById(R.id.sbWeight);
-        //dodanie lisenera do sbWeight
-        sbWeight.setOnSeekBarChangeListener(this);
+        // znajdziemy elementy
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
 
-        lParams1 = (LinearLayout.LayoutParams) btn1.getLayoutParams();
-        lParams2 = (LinearLayout.LayoutParams) btn2.getLayoutParams();
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        // przepisuje procedury obsługi
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.add(0, MENU_RESET_ID, 0, "Reset");
+        menu.add(0, MENU_QUIT_ID, 0, "Quit");
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case MENU_RESET_ID:
+                // oczyszczamy pola
+                etNum1.setText("");
+                etNum2.setText("");
+                tvResult.setText("");
+                break;
+            case MENU_QUIT_ID:
+                // wyjść z aplikacji
+                finish();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    //Ta funkcja jest wywoływana podczas przesówania elementu na elemencie SeekBaar
     @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        int leftValue = progress;
-        int rightValue = seekBar.getMax() - progress;
-        // dostosowywać  waga      lParams1.weight = leftValue;
+    public void onClick(View v) {
 
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
 
-        lParams1.weight = leftValue;
-        // wpisać wartośći zmiennych jako teksty przycisków
-        btn1.setText(String.valueOf(leftValue));
-        btn2.setText(String.valueOf(rightValue));
-    }
+        // Sprawdzanie pola na pustkę
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+        }
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
+        // czytaj EditText i wypełnić  zmienny jako liczbą
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
 
-    }
+        // zdefiniować wciśnięty przycisk i wykonać odpowiednią operację
+        // do oper zapiszmy operacji, a następnie będą wykorzystywane
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+        // tworzenia linii wyjściowy
+        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
 
     }
 }
