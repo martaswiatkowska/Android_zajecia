f22a938557b195e0b8cf2387c6174789335b4acb
Marta Świątkowska <martaswiatkowska8@gmail.com>
2017-12-27 21:57:15 +0100

    zadanie 20

--- app/src/main/java/com/example/marta/zadanie9/MainActivity.java
+++ app/src/main/java/com/example/marta/zadanie9/MainActivity.java
@@ -2,127 +2,97 @@ package com.example.marta.zadanie9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    final int MENU_RESET_ID = 1;
-    final int MENU_QUIT_ID = 2;
+public class MainActivity extends AppCompatActivity {
+    // Stale dla ID elementow menu
+    final int MENU_ALPHA_ID = 1;
+    final int MENU_SCALE_ID = 2;
+    final int MENU_TRANSLATE_ID = 3;
+    final int MENU_ROTATE_ID = 4;
+    final int MENU_COMBO_ID = 5;
 
-    EditText etNum1;
-    EditText etNum2;
-
-    Button btnAdd;
-    Button btnSub;
-    Button btnMult;
-    Button btnDiv;
-
-    TextView tvResult;
-
-    String oper = "";
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // znajdziemy elementy
-        etNum1 = (EditText) findViewById(R.id.etNum1);
-        etNum2 = (EditText) findViewById(R.id.etNum2);
+        tv = (TextView) findViewById(R.id.tv);
+        // zarejestruj menu kontekstowe dla komponentu tv
+        registerForContextMenu(tv);
+    }
 
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnSub = (Button) findViewById(R.id.btnSub);
-        btnMult = (Button) findViewById(R.id.btnMult);
-        btnDiv = (Button) findViewById(R.id.btnDiv);
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        switch (v.getId()) {
+            case R.id.tv:
+                // dodawanie elementów
+                menu.add(0, MENU_ALPHA_ID, 0, "alpha");
+                menu.add(0, MENU_SCALE_ID, 0, "scale");
+                menu.add(0, MENU_TRANSLATE_ID, 0, "translate");
+                menu.add(0, MENU_ROTATE_ID, 0, "rotate");
+                menu.add(0, MENU_COMBO_ID, 0, "combo");
+                break;
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
 
-        tvResult = (TextView) findViewById(R.id.tvResult);
-
-        // przepisuje procedury obsługi
-        btnAdd.setOnClickListener(this);
-        btnSub.setOnClickListener(this);
-        btnMult.setOnClickListener(this);
-        btnDiv.setOnClickListener(this);
-
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Animation anim = null;
+        // określić, który element został kliknięty
+        switch (item.getItemId()) {
+            case MENU_ALPHA_ID:
+                // tworzenie obiektu animacja z pliku anim/myalpha
+                anim = AnimationUtils.loadAnimation(this, R.anim.myalpha);
+                break;
+            case MENU_SCALE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myscale);
+                break;
+            case MENU_TRANSLATE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mytrans);
+                break;
+            case MENU_ROTATE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myrotate);
+                break;
+            case MENU_COMBO_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mycombo);
+                break;
+        }
+        // uruchomić animację dla komponent tv
+        tv.startAnimation(anim);
+        return super.onContextItemSelected(item);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
-        menu.add(0, MENU_RESET_ID, 0, "Reset");
-        menu.add(0, MENU_QUIT_ID, 0, "Quit");
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        switch (item.getItemId()) {
-            case MENU_RESET_ID:
-                // oczyszczamy pola
-                etNum1.setText("");
-                etNum2.setText("");
-                tvResult.setText("");
-                break;
-            case MENU_QUIT_ID:
-                // wyjść z aplikacji
-                finish();
-                break;
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        float num1 = 0;
-        float num2 = 0;
-        float result = 0;
-
-        // Sprawdzanie pola na pustkę
-        if (TextUtils.isEmpty(etNum1.getText().toString())
-                || TextUtils.isEmpty(etNum2.getText().toString())) {
-            return;
-        }
-
-        // czytaj EditText i wypełnić  zmienny jako liczbą
-        num1 = Float.parseFloat(etNum1.getText().toString());
-        num2 = Float.parseFloat(etNum2.getText().toString());
-
-        // zdefiniować wciśnięty przycisk i wykonać odpowiednią operację
-        // do oper zapiszmy operacji, a następnie będą wykorzystywane
-        switch (v.getId()) {
-            case R.id.btnAdd:
-                oper = "+";
-                result = num1 + num2;
-                break;
-            case R.id.btnSub:
-                oper = "-";
-                result = num1 - num2;
-                break;
-            case R.id.btnMult:
-                oper = "*";
-                result = num1 * num2;
-                break;
-            case R.id.btnDiv:
-                oper = "/";
-                result = num1 / num2;
-                break;
-            default:
-                break;
-        }
-
-        // tworzenia linii wyjściowy
-        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
-
     }
 }
 
