32b2c8fef7e053d4cbe23ffc5d3276dd6f9501a1
Marta Świątkowska <martaswiatkowska8@gmail.com>
2018-01-10 20:00:41 +0100

    Zadanie 23 z wykl 6

--- app/src/main/java/com/example/marta/zadanie9/MainActivity.java
+++ app/src/main/java/com/example/marta/zadanie9/MainActivity.java
@@ -17,21 +17,63 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     // identyfikator notyfikacji
+    // Zmienna, elementem sterowania "pola wprowadź"
+    private EditText mInputEditText;
+
+    // Stały konwersji walut
+    private final double rateUsd  = 3.66;
+    private final double rateEur  = 4.05;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Dostać linka do słońca
-        ImageView sun = (ImageView) findViewById(R.id.sun);
-        // Animacje dla wschodu słońca
-        Animation sunRise = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
-        // Podłączanie animacji do pożądanego View
-        sun.startAnimation(sunRise);
+        //otrzymać dostęp do elementu zarządzanie "pola wprowadź" (tutaj wprowadzony suma w złotych)
+        mInputEditText = (EditText) findViewById(R.id.editText);
+    }
+
+    private float convertZlotyToEuro(float a) {
+        return (float) (a / rateEur);
+    }
+
+    private float convertZlotyToUsd(float a) {
+        return (float) (a / rateUsd);
+    }
+
+    // Przetwarzanie kliknięcia na przycisk Convert
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.buttonConverter:
+                RadioButton euroRadioButton = (RadioButton) findViewById(R.id.radioButtonEuro);
+
+                // sprawdzić pola wprowadzania na pustkę, jeśli długość 0, wyświetli komunikat
+                if (mInputEditText.getText().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Enter amount in zloty",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // zadeklarować zmienną typu realnej liczby pojedynczej precyzji(float)
+                // Float.parse - przekonwertować ciąg do liczby
+                float inputValue = Float.parseFloat(mInputEditText.getText().toString());
+                if (euroRadioButton.isChecked()) {
+                    // konwersji liczb na łańcuchy i przypisać do pola wprowadzania,
+                    // pokazać użytkownikowi wynik
+                    mInputEditText.setText(String.valueOf(convertZlotyToEuro(inputValue)));
+                } else {
+                    mInputEditText.setText(String.valueOf(convertZlotyToUsd(inputValue)));
+                }
+                break;
+        }
     }
 }
 
