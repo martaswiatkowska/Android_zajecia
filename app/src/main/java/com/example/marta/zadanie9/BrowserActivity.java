b3a07b5d7dc40836643c3b37e0b89177d503ea20
Marta Świątkowska <martaswiatkowska8@gmail.com>
2018-01-16 01:22:14 +0100

    zadanie32

---
+++ app/src/main/java/com/example/marta/zadanie9/BrowserActivity.java
@@ -0,0 +1,22 @@
+package com.example.marta.zadanie9;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+
+public class BrowserActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        Uri data = getIntent().getData();
+        webView.loadUrl(data.toString());
+    }
+}
--- app/src/main/java/com/example/marta/zadanie9/MainActivity.java
+++ app/src/main/java/com/example/marta/zadanie9/MainActivity.java
@@ -12,11 +12,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity{
 
-    Button btnWeb;
-    Button btnMap;
-    Button btnCall;
 
     /** Called when the activity is first created. */
     @Override
@@ -24,37 +21,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnWeb = (Button) findViewById(R.id.btnWeb);
-        btnMap = (Button) findViewById(R.id.btnMap);
-        btnCall = (Button) findViewById(R.id.btnCall);
-
-        btnWeb.setOnClickListener(this);
-        btnMap.setOnClickListener(this);
-        btnCall.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent intent;
-        switch (v.getId()) {
-            case R.id.btnWeb:
-                //ustawienie jaka strona internetowa ma się wyświetlić w przeglądarce
-                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
-                startActivity(intent);
-                break;
-            case R.id.btnMap:
-                intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                //ustawienie położenia jakie ma zostać wyświetlone w aplikacji mapGoogle
-                intent.setData(Uri.parse("geo:55.754283,37.62002"));
-                startActivity(intent);
-                break;
-            case R.id.btnCall:
-                intent = new Intent(Intent.ACTION_DIAL);
-                //Ustawienie numeru telefonu
-                intent.setData(Uri.parse("tel:12345"));
-                startActivity(intent);
-                break;
-        }
+        (findViewById(R.id.btnWeb)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.agh.edu.pl")));
+            }
+        });
     }
 }
