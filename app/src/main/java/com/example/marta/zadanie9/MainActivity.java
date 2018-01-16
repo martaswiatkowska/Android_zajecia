c3f35d25ba9f2dd81875568d3a158174be8a38bd
Marta Świątkowska <martaswiatkowska8@gmail.com>
2018-01-16 01:50:20 +0100

    zadanie 34.1 PATRZ W LOGIgit add .git add .

--- app/src/main/java/com/example/marta/zadanie9/MainActivity.java
+++ app/src/main/java/com/example/marta/zadanie9/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.marta.zadanie9;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,14 +20,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements View.OnClickListener{
-    EditText etText;
-    Button btnSave, btnLoad;
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    SharedPreferences sPref;
+    final String LOG_TAG = "myLogs";
 
-    final String SAVED_TEXT = "saved_text";
+    Button btnAdd, btnRead, btnClear;
+    EditText etName, etEmail;
 
+    DBHelper dbHelper;
 
     /** Called when the activity is first created. */
     @Override
@@ -29,49 +35,112 @@ public class MainActivity extends Activity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etText = (EditText) findViewById(R.id.etText);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
 
-        btnSave = (Button) findViewById(R.id.btnSave);
-        btnSave.setOnClickListener(this);
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
 
-        btnLoad = (Button) findViewById(R.id.btnLoad);
-        btnLoad.setOnClickListener(this);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
 
-        loadText();
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+
+        // utworzyć obiekt do tworzenia i zarządzania wersjami bazy danych
+        dbHelper = new DBHelper(this);
     }
+
 
     @Override
     public void onClick(View v) {
+
+        // tworzenie obiektu dla danych
+
+        ContentValues cv = new ContentValues();
+
+        // uzyskanie danych z pola wprowadzania
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+
+        // podłączenie do bazy danych
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+
         switch (v.getId()) {
-            case R.id.btnSave:
-                saveText();
+            case R.id.btnAdd:
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+                // przygotowanie danych do wstawiania jako pary: nazwa kolumny -
+                // wartość
+                cv.put("name", name);
+                cv.put("email", email);
+                // wstawić rekord i dostać go ID
+                long rowID = db.insert("mytable", null, cv);
+                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
                 break;
-            case R.id.btnLoad:
-                loadText();
+            case R.id.btnRead:
+                Log.d(LOG_TAG, "--- Rows in mytable: ---");
+                // robimy zapytanie wszystkich danych z tabeli mytable, dostajemy Cursor
+                Cursor c = db.query("mytable", null, null, null, null, null, null);
+
+                // umieścić pozycję kursora na pierwszy linie próby
+                // jeśli w próbie nie ma rekordów, return false
+                if (c.moveToFirst()) {
+
+                    // ustalać numery kolumnów wg nazwy w próbie
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
+
+                    do {
+                        // uzyskania wartości według numerów
+                        //    kolumnów i napisać wszystko w log
+                        Log.d(LOG_TAG,
+                                "ID = " + c.getInt(idColIndex) +
+                                        ", name = " + c.getString(nameColIndex) +
+                                        ", email = " + c.getString(emailColIndex));
+                        // przejść do następnej linii
+                        // a jeśli nie ma następny (obecny - ostatnia),  false -
+                        // wyjść z pętli
+                    } while (c.moveToNext());
+                } else
+                    Log.d(LOG_TAG, "0 rows");
+                c.close();
                 break;
-            default:
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "--- Clear mytable: ---");
+                // удаляем все записи
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
                 break;
+        }
+        // zamknąć połączenie z bazą danych
+        dbHelper.close();
+    }
+
+
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            // konstruktor superklasa
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+            // tworzenia tabeli z polami
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + "email text" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
         }
     }
 
-    void saveText() {
-        sPref = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor ed = sPref.edit();
-        ed.putString(SAVED_TEXT, etText.getText().toString());
-        ed.commit();
-        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
-    }
-
-    void loadText() {
-        sPref = getPreferences(MODE_PRIVATE);
-        String savedText = sPref.getString(SAVED_TEXT, "");
-        etText.setText(savedText);
-        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        saveText();
-        super.onDestroy();
-    }
 }
